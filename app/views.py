a3150ff49bbec0b32988306a450e3f8780c9b594
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 11:10:19 +0100

    go

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template('public/index.html')
+
+@app.route("/about")
+def about():
+    return render_template('public/about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('public/contact.html')
+
+@app.route("/services")
+def services():
+    return render_template('public/services.html')
+
